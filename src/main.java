c76072fb4426d2972f5dbca6b188de0f57fb5b99
Mañana <Maana@Aula1-14>
2012-01-18 12:32:12 +0100

    prueba f.palacios

--- src/main.java
+++ src/main.java
@@ -7,6 +7,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Subversion, tu antes molabas");
+		System.out.println("weee");
 
 	}
 
