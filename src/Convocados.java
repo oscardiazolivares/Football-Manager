c87dc769faecfc02bd756257508ac9e513208bba
Óscar Díaz Olivares <oscardiazolivares@gmail.com>
2012-01-20 13:54:31 +0100

    *Creadas las clases Convocados, Futbolista y Plantilla (incompletas)

---
+++ src/Convocados.java
@@ -0,0 +1,139 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Convocados {
+	int[] formacion = new int[3];
+	Futbolista portero;
+	List<Futbolista> defensas = new ArrayList<Futbolista>();
+	List<Futbolista> centrocampistas = new ArrayList<Futbolista>();
+	List<Futbolista> delanteros = new ArrayList<Futbolista>();
+	Plantilla plantilla;
+	
+	public Convocados (String nombreEquipo) {
+		if (nombreEquipo == "betis") {
+			//Si el equipo es el betis, cargo los jugadores del betis en el arraylist
+		}
+	}
+	
+	public int calcularAtaque() {
+		double ataqueTotal=0;
+		//Recorro la lista de delanteros desde el 0 hasta el tamaño de la lista
+		int SumaAtDel = 0;
+		for(int i=0;i<delanteros.size();i++) {
+			//Sumo consecutivamente el ataque de cada delantero
+			SumaAtDel = SumaAtDel + delanteros.get(i).ataque;
+		}
+		//Recorro la lista de centrocampistas desde el 0 hasta el tamaño de la lista
+		int SumaAtCen = 0;
+		for(int i=0;i<centrocampistas.size();i++) {
+			//Sumo consecutivamente el ataque de cada centrocampista
+			SumaAtCen = SumaAtCen + centrocampistas.get(i).ataque;
+		}
+		//Recorro la lista de defensas desde el 0 hasta el tamaño de la lista
+		int SumaAtDef = 0;
+		for(int i=0;i<defensas.size();i++) {
+			//Sumo consecutivamente el ataque de cada defensa
+			SumaAtDef = SumaAtDef + defensas.get(i).ataque;
+		}
+		//AtaqueTotal = [(SumaAtDel*0.5)+(SumaAtCen*0.3)+(SumaAtDef*0.2)]/10
+		ataqueTotal= (SumaAtDel*0.5+SumaAtCen*0.3+SumaAtDef*0.2)/10;
+		return (int)ataqueTotal;
+	}
+	
+	public int calcularDefensa() {
+		double defensaTotal=0;
+		//Recorro la lista de delanteros desde el 0 hasta el tamaño de la lista
+		int SumaDefDel = 0;
+		for(int i=0;i<delanteros.size();i++) {
+			//Sumo consecutivamente la defensa de cada delantero
+			SumaDefDel = SumaDefDel + delanteros.get(i).defensa;
+		}
+		//Recorro la lista de centrocampistas desde el 0 hasta el tamaño de la lista
+		int SumaDefCen = 0;
+		for(int i=0;i<centrocampistas.size();i++) {
+			//Sumo consecutivamente la defensa de cada centrocampista
+			SumaDefCen = SumaDefCen + centrocampistas.get(i).defensa;
+		}
+		//Recorro la lista de defensas desde el 0 hasta el tamaño de la lista
+		int SumaDefDef = 0;
+		for(int i=0;i<defensas.size();i++) {
+			//Sumo consecutivamente la defensa de cada defensa
+			SumaDefDef = SumaDefDef + defensas.get(i).defensa;
+		}
+
+		//DefensaTotal = [(SumaDefDel*0.5)+(SumaDefCen*0.3)+(SumaDefDef*0.2)]/10
+		defensaTotal= (SumaDefDel*0.5+SumaDefCen*0.3+SumaDefDef*0.2)/10;
+		return (int)defensaTotal;
+	}
+	
+	public int calcularTecnica() {
+		double tecnicaTotal=0;
+		//Recorro la lista de delanteros desde el 0 hasta el tamaño de la lista
+		int SumaTecDel = 0;
+		for(int i=0;i<delanteros.size();i++) {
+			//Sumo consecutivamente la tecnica de cada delantero
+			SumaTecDel = SumaTecDel + delanteros.get(i).tecnica;
+		}
+		//Recorro la lista de centrocampistas desde el 0 hasta el tamaño de la lista
+		int SumaDefCen = 0;
+		for(int i=0;i<centrocampistas.size();i++) {
+			//Sumo consecutivamente la defensa de cada centrocampista
+			SumaDefCen = SumaDefCen + centrocampistas.get(i).defensa;
+		}
+		//Recorro la lista de defensas desde el 0 hasta el tamaño de la lista
+		int SumaDefDef = 0;
+		for(int i=0;i<defensas.size();i++) {
+			//Sumo consecutivamente la defensa de cada defensa
+			SumaDefDef = SumaDefDef + defensas.get(i).defensa;
+		}
+		//DefensaTotal = [(SumaDefDel*0.5)+(SumaDefCen*0.3)+(SumaDefDef*0.2)]/10
+		defensaTotal= (SumaDefDel*0.5+SumaDefCen*0.3+SumaDefDef*0.2)/10;
+		return (int)defensaTotal;
+	}
+
+	private int[] getFormacion() {
+		return formacion;
+	}
+
+	private void setFormacion(int[] formacion) {
+		this.formacion = formacion;
+	}
+
+	private Futbolista getPortero() {
+		return portero;
+	}
+
+	private void setPortero(Futbolista portero) {
+		this.portero = portero;
+	}
+
+	private List<Futbolista> getDefensas() {
+		return defensas;
+	}
+
+	private void setDefensas(List<Futbolista> defensas) {
+		this.defensas = defensas;
+	}
+
+	private List<Futbolista> getCentrocampistas() {
+		return centrocampistas;
+	}
+
+	private void setCentrocampistas(List<Futbolista> centrocampistas) {
+		this.centrocampistas = centrocampistas;
+	}
+
+	private List<Futbolista> getDelanteros() {
+		return delanteros;
+	}
+
+	private void setDelanteros(List<Futbolista> delanteros) {
+		this.delanteros = delanteros;
+	}
+
+	private Plantilla getPlantilla() {
+		return plantilla;
+	}
+	
+}
---
+++ src/Futbolista.java
@@ -0,0 +1,153 @@
+
+public class Futbolista {
+	int id;
+	String nombre;
+	boolean lesion;
+	int tarjetas;
+	int ataque;
+	int defensa;
+	int tecnica;
+	int resistenciaLesiones;
+	boolean sancion;
+	int agresividad;
+	int fatiga;
+	int habilidadPortero;
+	String posicion;
+	boolean expulsion;
+	
+	public Futbolista(int id, String nombre, boolean lesion, int tarjetas,
+			int ataque, int defensa, int tecnica, int resistenciaLesiones,
+			boolean sancion, int agresividad, int fatiga, int habilidadPortero,
+			String posicion, boolean expulsion) {
+		this.id = id;
+		this.nombre = nombre;
+		this.lesion = lesion;
+		this.tarjetas = tarjetas;
+		this.ataque = ataque;
+		this.defensa = defensa;
+		this.tecnica = tecnica;
+		this.resistenciaLesiones = resistenciaLesiones;
+		this.sancion = sancion;
+		this.agresividad = agresividad;
+		this.fatiga = fatiga;
+		this.habilidadPortero = habilidadPortero;
+		this.posicion = posicion;
+		this.expulsion = expulsion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public boolean isLesion() {
+		return lesion;
+	}
+
+	private void setLesion(boolean lesion) {
+		this.lesion = lesion;
+	}
+
+	public int getTarjetas() {
+		return tarjetas;
+	}
+
+	private void setTarjetas(int tarjetas) {
+		this.tarjetas = tarjetas;
+	}
+
+	public int getAtaque() {
+		return ataque;
+	}
+
+	private void setAtaque(int ataque) {
+		this.ataque = ataque;
+	}
+
+	public int getDefensa() {
+		return defensa;
+	}
+
+	private void setDefensa(int defensa) {
+		this.defensa = defensa;
+	}
+
+	public int getTecnica() {
+		return tecnica;
+	}
+
+	private void setTecnica(int tecnica) {
+		this.tecnica = tecnica;
+	}
+
+	public int getResistenciaLesiones() {
+		return resistenciaLesiones;
+	}
+
+	private void setResistenciaLesiones(int resistenciaLesiones) {
+		this.resistenciaLesiones = resistenciaLesiones;
+	}
+
+	public boolean isSancion() {
+		return sancion;
+	}
+
+	private void setSancion(boolean sancion) {
+		this.sancion = sancion;
+	}
+
+	public int getAgresividad() {
+		return agresividad;
+	}
+
+	private void setAgresividad(int agresividad) {
+		this.agresividad = agresividad;
+	}
+
+	public int getFatiga() {
+		return fatiga;
+	}
+
+	private void setFatiga(int fatiga) {
+		this.fatiga = fatiga;
+	}
+
+	public int getHabilidadPortero() {
+		return habilidadPortero;
+	}
+
+	private void setHabilidadPortero(int habilidadPortero) {
+		this.habilidadPortero = habilidadPortero;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+
+	public boolean isExpulsion() {
+		return expulsion;
+	}
+
+	private void setExpulsion(boolean expulsion) {
+		this.expulsion = expulsion;
+	}
+	
+	
+
+
+}
